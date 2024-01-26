30ada63797b96e8f7b859aa63fc973d176c94cad
VlastimilVasut <vasutvlastimil@gmail.com>
2024-01-26 14:49:04 +0100

    It can now tell the difference between logged in user and other users

--- Nav/Nav.php
+++ Nav/Nav.php
@@ -13,7 +13,7 @@
     <a class="navmenu" href="../Home/Home.php">
         Messages
     </a><br>
-    <a class="navmenu" href="../Profile/Profile.php/?username=<?php echo $_SESSION["username"]; ?>">
+    <a class="navmenu" href="../Profile/Profile.php?username=<?php echo $_SESSION["username"]; ?>">
         Profile
     </a><br>
 </nav>
--- Profile/Profile.php
+++ Profile/Profile.php
@@ -1,16 +1,27 @@
 <?php
-   session_start();
-   $username = $_SESSION["username"];
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../UniversalCSS/UniversalStyles.css">
-</head>
-<body>
+session_start();
+include("../DbHandler.php");
+include_once("../ConnectionChecker.php");
+
+$username = $_SESSION["username"];
+Db::connect("localhost", "sin", "root", "");
+
+$userData = Db::queryOne("SELECT * FROM users WHERE Username=?", $username);
+
+if ($userData) {
+    $storedUsername = $userData['Username'];
     
-</body>
-</html>
+    echo "Username from the database: $storedUsername";
+
+    // Get the username from the URL
+    $URL_username = $_GET['username'] ?? null;
+
+    if ($URL_username && $URL_username === $username) {
+        include("MyProfile.php");
+    } else {
+        include("NotMyProfile.php");
+    }
+} else {
+    echo "User not found in the database.";
+}
+?>
