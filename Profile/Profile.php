55d4e81c32e32a9af4fbe2c8aaa667082091bde6
VlastimilVasut <vasutvlastimil@gmail.com>
2024-01-27 11:13:28 +0100

    Now displaying the right Username and Name

--- Profile/Profile.php
+++ Profile/Profile.php
@@ -1,6 +1,14 @@
 <?php
     session_start();
     require("../ConnectionChecker.php");
+    require("../DbHandler.php");
+    $username = $_GET['username'];
+    Db::connect("localhost", "sin", "root", "");
+    $Users = Db::queryAll("SELECT * FROM  users WHERE Username=?", $username);
+    foreach($Users as $User)
+    {
+        $name = $User["Name"];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,16 +24,20 @@
     <?php
         require("../Nav/Nav.php");
     ?>
-    <img src="../DefaultPFP/DefaultPFP.png" class="PFP">
     <div class="whereamI">
            <label class="lblMyProfile">Profile</label>
     </div>
     <div class="User">
+        <img src="../DefaultPFP/DefaultPFP.png" class="PFP">
         <div class="Name">
-            Name
+            <?php
+                echo $name;
+            ?>
         </div>
         <div class="Username">
-            @Username
+            <?php
+                echo $username;
+            ?>
         </div>
     </div>
 </body>
