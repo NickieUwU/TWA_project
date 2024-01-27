72189ffb9a9a37f35900c575b218f57ec17b9391
VlastimilVasut <vasutvlastimil@gmail.com>
2024-01-27 14:55:16 +0100

    Now I need to actually perform the action on button click

--- Profile/Profile.php
+++ Profile/Profile.php
@@ -2,7 +2,7 @@
     session_start();
     require("../ConnectionChecker.php");
     require("../DbHandler.php");
-    $username = $_GET['username']; //then grab $_SESSION and compare this to this username variable to see if it's me or not
+    $username = $_GET['username'];
     Db::connect("localhost", "sin", "root", "");
     $Users = Db::queryAll("SELECT * FROM  users WHERE Username=?", $username);
     foreach($Users as $User)
@@ -23,44 +23,66 @@
     <link rel="stylesheet" href="Profile.css">
 </head>
 <body>
-    <?php
-        require("../Nav/Nav.php");
-    ?>
-    <div class="whereamI">
-           <label class="lblMyProfile">Profile</label>
-    </div>
-    <div class="User">
-        <img src="../DefaultPFP/DefaultPFP.png" class="PFP">
-        <div class="Name">
-            <?php
-                echo $name;
-            ?>
+    <form method="post" action="Profile.php">
+        <?php
+            require("../Nav/Nav.php");
+        ?>
+        <div class="whereamI">
+            <label class="lblMyProfile">Profile</label>
         </div>
-        <div class="Username">
-            <?php
-                echo $username;
-            ?>
-        </div>
-        <div class="Bio">
-            <?php
-                echo $bio;
-            ?>
-        </div>
-        <div class="JoinedDate">
-            <?php echo "Joined $joined"; ?>
-        </div>
-        <div class="Action">
+        <div class="User">
+            <img src="../DefaultPFP/DefaultPFP.png" class="PFP">
+            <div class="Name">
                 <?php
-                    if($_SESSION["username"] == $username)
-                    {
-                        echo "<button type='button'>edit</button>";
-                    }
-                    else
-                    {
-                        echo "<button type='button'>follow</button>";
-                    }
+                    echo $name;
                 ?>
+            </div>
+            <div class="Username">
+                <?php
+                    echo $username;
+                ?>
+            </div>
+            <div class="Bio">
+                <?php
+                    echo $bio;
+                ?>
+            </div>
+            <div class="JoinedDate">
+                <?php echo "Joined $joined"; ?>
+            </div>
+            <div class="Action">
+                
+                        <?php
+                            if($_SESSION["username"] == $username)
+                            {
+                                echo "<input type='submit' name='edit' value='edit'>";
+                            }
+                            else
+                            {
+                                echo "<input type='submit' name='follow' value='follow'>";
+                            }
+                        ?>
+                    
+            </div>
         </div>
-    </div>
+    </form>
 </body>
 </html>
+
+<?php
+    if($_POST)
+    {
+        $btnEdit = $_POST["edit"];
+        $btnFollow = $_POST["follow"];
+
+        if(isset($btnEdit))
+        {
+            echo "editing";
+        }
+
+        elseif(isset($btnFollow))
+        {
+            echo "following";
+        }
+    }
+?>
