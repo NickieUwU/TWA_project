9cc563dd95559effa79d1563f088e0f694130915
VlastimilVasut <vasutvlastimil@gmail.com>
2024-01-27 22:07:32 +0100

    Started working around profile edit

--- ProfileEdit/ProfileEdit.php
+++ ProfileEdit/ProfileEdit.php
@@ -3,11 +3,7 @@
     require("../ConnectionChecker.php");
     require("../DbHandler.php");
     $username = $_GET["username"];
-    if($_SESSION["username"] != $username)
-    {
-        header("location: ../Home/Home.php");
-        exit;
-    }
+    require("../ValidateUser.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,8 +12,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../UniversalCSS/UniversalStyles.css">
-</head>
+    <link rel="stylesheet" href="../Nav/Nav.css">
+    <link rel="stylesheet" href="ProfileEdit.css">
 <body>
-    
+    <?php 
+        include("../Nav/Nav.php");
+    ?>
+    <div class="whereamI">
+           <label class="lblMyProfile">Edit profile</label>
+    </div>
+    <div class="EditUsernameDiv">
+        <form action="ProfileEdit.php?username=$username" method="post">
+
+        </form>
+    </div>
 </body>
 </html>
