3f14b8d405405bb6c916fff3dbf2b83e7dbbd81f
VlastimilVasut <vasutvlastimil@gmail.com>
2024-01-28 15:09:32 +0100

    Gotta recreate the home page with bootstrap

--- Home/Home.php
+++ Home/Home.php
@@ -1,9 +1,7 @@
 <?php
     session_start();
     include_once("../ConnectionChecker.php");
-
     require("../Nav/Nav.php");
-
     require("../DbHandler.php");
     Db::connect("localhost", "sin", "root", "");
     $Users = Db::queryAll("SELECT * FROM users");
@@ -38,46 +36,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home / Sin</title>
-    <link rel="stylesheet" href="../UniversalCSS//UniversalStyles.css">
+    <title>Document</title>
     <link rel="stylesheet" href="../Nav/Nav.css">
-    <link rel="stylesheet" href="Home.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+        crossorigin="anonymous">
 </head>
 <body>
-    <div class="whereamI">
-           <label onclick="refreshPage()" class="lblHome">Home</label>
-            <script>
-                function refreshPage() 
-                {
-                    location.reload();
-                }
-            </script>
-    </div>
-    <div class="Posts">
-        <?php
-            echo "<a href='../Profile/Profile.php?username=$Username'>
-                    <img src='../DefaultPFP/DefaultPFP.png' class='PFP'>
-                  </a>";
-        ?>
-        <div class="Name">
-           <?php
-               echo "<a href='../Profile/Profile.php?username=$Username' class='Name'>$Name</a>";
-           ?>
-        </div>
-        <div class="Username">
-           <?php
-              echo $Username;
-           ?>
-        </div>
-        <div class="Post">
-                <?php 
-                    echo $Content;
-                ?>
-        </div>
-        <form action="Home.php" method="get">
-        <button type="submit" name="logout">Log out</button>
-    </form>
-    </div>
-
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
+        crossorigin="anonymous">
+    </script>
 </body>
 </html>
