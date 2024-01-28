897e2969c3cf891179b6ff6e86212e4615c10ca7
VlastimilVasut <vasutvlastimil@gmail.com>
2024-01-28 10:27:01 +0100

    Fixed validating the user

--- ValidateUser.php
+++ ValidateUser.php
@@ -1,7 +1,7 @@
 <?php
     if($_SESSION["username"] != $username)
     {
-        header("location: ../ProfileEdit/ProfileEdit.php?username=".$_SESSION["username"]);
+        header("location: ?username=".$_SESSION["username"]);
         exit;
     }
 ?>
