1e7071bf7643b529a7a21fb984d379dcc086ca7d
VlastimilVasut <vasutvlastimil@gmail.com>
2024-01-29 20:01:22 +0100

    Made an expand post window

---
+++ ExpandedPost/ExpandedPost.php
@@ -0,0 +1,16 @@
+<?php
+    require("../DbHandler.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../UniversalCSS/UniversalStyles.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../Nav/Nav.css?v=<?php echo time(); ?>">
+</head>
+<body>
+    <?php include("../Nav/Nav.php"); ?>
+</body>
+</html>
--- Post/Post.php
+++ Post/Post.php
@@ -37,33 +37,46 @@
         $IsLiked = $Like["Liked"];
     }
 
-    echo $IsLiked;
+    function liked($IsLiked, $LoggedID, $Post_ID)
+    {
+        if($IsLiked == 0)
+            {
+                $IsLiked == 1;
+                Db::query("INSERT INTO likes WHERE ID=? AND Post_ID=?", $LoggedID, $Post_ID);
+            }
+            if($IsLiked == 1)
+            {
+                $IsLiked = 0;
+            }
+    }
+
 ?>
-    
-    <div class="post">
-        <img src="../DefaultPFP/DefaultPFP.png" alt="Profile picture" class="PFP">
-        <div class="post-info">
-            <p class="name"><?php echo $Name; ?></p>
-            <p class="handler"><?php echo $Username; ?></p>
-            
+<div class="post">
+    <img src="../DefaultPFP/DefaultPFP.png" alt="Profile picture" class="PFP">
+    <div class="post-info">
+        <p class="name"><?php echo $Name; ?></p>
+        <p class="handler"><?php echo $Username; ?></p>
+        
+    </div>
+    <div class="date">
+        <p class="date"><?php echo $Date; ?></p>
+    </div>
+    <textarea class="post-text" readonly><?php echo $Content; ?></textarea>
+    <div class="actions" id="actionID">
+        <div class="heart" id="heart">
+            <?php
+            if($IsLiked == 0) {
+                echo "<i class='bi bi-heart' id='likeIcon' onclick='ExecuteLike()'></i>";
+            }
+            if($IsLiked == 1) {
+                echo "<i class='bi bi-heart-fill' id='likeIcon' onclick='ExecuteLike()'></i>";
+            }
+            ?>  
         </div>
-        <div class="date">
-            <p class="date"><?php echo $Date; ?></p>
-        </div>
-        <textarea class="post-text" readonly><?php echo $Content; ?></textarea>
-        <div class="actions" id="actionID">
-            <div onclick="changeIcon()" class="heart" id="heart">
-                <?php
-                    if($IsLiked == 0)
-                    {
-                        echo "<i class='bi bi-heart'></i>";
-                    }
-                    if($IsLiked == 1)
-                    {
-                        echo "<i class='bi bi-heart-fill'></i>";
-                    }
-                ?>  
-            </div>
-            
+        <div class="comments">
+           <a href="../ExpandedPost/ExpandedPost.php/<?php echo $Post_ID?>">
+                <i class="bi bi-chat-left-dots-fill"></i>
+           </a> 
         </div>
     </div>
+</div>
