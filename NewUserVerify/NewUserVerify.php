ceb20d98d5540fad8fa3d851ce5ce8a10061fa3a
Nikča <136233480+NickieUwU@users.noreply.github.com>
2024-02-01 13:11:38 +0100

    Update NewUserVerify.php
    Added an h1

--- NewUserVerify/NewUserVerify.php
+++ NewUserVerify/NewUserVerify.php
@@ -9,9 +9,10 @@
     <title>Document</title>
     <script src="NewUserVerify.js"></script>
     <link rel="stylesheet" href="../UniversalCSS/UniversalStyles.css">
-    <link rel="stylesheet" href="../NewUserVerify/NewUserVerify.css">
+    <link rel="stylesheet" href="../NewUserVerify/NewUserVerify.css?v=<?php echo time(); ?>">
 </head>
 <body onload="changecontent()">
+    <h1>Verify your identity</h1>
     <form class="form_verify" action="NewUserVerify.php" method="post">
         <div id="verification_content" class="verification"></div>
         <input type="radio" name="verification_radio" value="e-mail" id="email" onchange="changecontent()" checked><label class="typeText">E-mail</label><br>
