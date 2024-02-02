c3a8f338a5071c0d0d0cd4f0ad73b2811e5cb7af
VlastimilVasut <vasutvlastimil@gmail.com>
2024-02-02 14:20:02 +0100

    Working on search

--- Search/Search.php
+++ Search/Search.php
@@ -2,6 +2,11 @@
     session_start();
     require("../DbHandler.php");
     require("../ConnectionChecker.php");
+
+    if(isset($_GET["searchbar"]))
+    {
+        $search = $_GET["searchbar"];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,11 +24,30 @@
     <div class="whereamI">
         <label class="lblMyProfile">Search</label>
     </div>
-    <form action="Search.php" method="get">
+    <form action="Search.php" method="get" id="FormSearch">
         <div class="search">
-            <input type="search" class="srchbar" name="searchbar" placeholder="Search">
+            <input type="search" class="srchbar" name="searchbar" placeholder="Search" autocomplete="off">
             <input type="submit" value="Search" id="btnSearch">
         </div>
     </form>
+    <div class="SearchResults"></div>
 </body>
 </html>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+    document.getElementById("FormSearch").addEventListener("submit", (e) => {
+        e.preventDefault();
+        let form = e.target;
+        let formData = new FormData(form);
+
+        let xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action, true);
+
+        xhr.onload = () => {
+            if(xhr.status === 200){
+                
+            }
+        };
+        xhr.send(formData);
+    });
+</script>
