02c8a6463c1a93331ce8f3f85f49c6563e5bef2e
VlastimilVasut <vasutvlastimil@gmail.com>
2024-02-02 13:22:45 +0100

    Modified a nav little and now I am working on searching a user

--- Nav/Nav.php
+++ Nav/Nav.php
@@ -1,15 +1,21 @@
 <nav>
     <a class="navmenu" href="../Home/Home.php">
-        Home
+        <i class="bi bi-house-door-fill"></i> Home
     </a><br>
+    <a class="navmenu" href="../Search/Search.php?username=<?php echo $_SESSION["username"]; ?>">
+        <i class="bi bi-search"></i> Search
+    </a>
     <a class="navmenu" href="../CreatePost/CreatePost.php?username=<?php echo $_SESSION["username"];?>">
-        Create
+        <i class="bi bi-pencil-fill"></i> Create
     </a><br>
     <a class="navmenu" href="../Messages/Messages.php?username=<?php echo $_SESSION["username"]; ?>">
-    <i class="bi bi-send"></i>Messages
+        <i class="bi bi-send"></i> Messages
     </a><br>
+    <a class="navmenu" href="../Notifications/Notifications.php?username=<?php echo $_SESSION["username"]; ?>">
+        <span class="bi bi-bell" style="font-size: 2rem;"></span> Notifications
+    </a>
     <a class="navmenu" href="../Profile/Profile.php?username=<?php echo $_SESSION["username"];?>">
-        Profile
+        <i class="bi bi-person-fill"></i> Profile
     </a><br>
 
     <?php
