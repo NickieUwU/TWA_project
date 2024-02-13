d4e3c069516dd8926ce18617914578587a0351fa
VlastimilVasut <vasutvlastimil@gmail.com>
2024-02-13 18:14:57 +0100

    Moving the js to its new seperate file

---
+++ Messages/Messages.js
@@ -0,0 +1,4 @@
+document.getElementById("NewMessageRedirect").addEventListener("click", function() {
+    let bg = document.querySelector(".layout");
+    bg.innerHTML = "<div class='ChooseUser'></div>";
+});
