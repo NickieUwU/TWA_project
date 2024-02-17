620ae24ebce56677a5fd6c6ba557dd69b307df6e
VlastimilVasut <vasutvlastimil@gmail.com>
2024-02-17 16:07:03 +0100

    Made 404 error page

---
+++ 404/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../UniversalCSS/UniversalStyles.css">
+    <link rel="stylesheet" href="../Nav/Nav.css">
+    <link rel="stylesheet" href="404.css">
+</head>
+<body>
+    <?php
+        include("../Nav/Nav.php");
+    ?>
+    <div class="Error404Div">
+        Error 404 user was not found
+    </div>
+</body>
+</html>
