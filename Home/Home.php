68e21f6f2aca051091e0db3c3b58efe2aad700df
VlastimilVasut <vasutvlastimil@gmail.com>
2024-02-29 14:44:44 +0100

    Home now shows correct text in the tab (kinda forgot about it)

--- Home/Home.php
+++ Home/Home.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home / Sin</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="../UniversalCSS/UniversalStyles.css">
     <link rel="stylesheet" href="../Nav/Nav.css?v=<?php echo time(); ?>">
