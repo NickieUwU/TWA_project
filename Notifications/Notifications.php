6fe4bb28fca8873c7f67de610bca5198774c6075
VlastimilVasut <vasutvlastimil@gmail.com>
2024-03-02 17:39:06 +0100

    Setting up notifications

--- Notifications/Notifications.php
+++ Notifications/Notifications.php
@@ -9,8 +9,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notifications / Sin</title>
+    <link rel="stylesheet" href="Notifications.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../UniversalCSS/UniversalStyles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../Nav/Nav.php"> 
 </head>
 <body>
-    
+    <div class="whereamI">
+           <label class="lblMyProfile">Notifications</label>
+    </div>
+    <?php
+        include("../Nav/Nav.php");
+    ?>
 </body>
 </html>
