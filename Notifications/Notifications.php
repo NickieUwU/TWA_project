28016961cb0149819ec3d726af1d8fe0b3e1d513
VlastimilVasut <vasutvlastimil@gmail.com>
2024-03-02 17:56:06 +0100

    Working around notifications

--- Notifications/Notifications.php
+++ Notifications/Notifications.php
@@ -2,6 +2,19 @@
     session_start();
     require("../DbHandler.php");
     require("../ConnectionChecker.php");
+    Db::connect("localhost", "sin", "root", "");
+    $LoggedUsers = Db::queryAll("SELECT * FROM users WHERE username=?", $_SESSION["username"]);
+    foreach($LoggedUsers as $LoggedUser)
+    {
+        $LoggedID = $LoggedUser["ID"];
+    }
+    $follows = Db::queryAll("SELECT * FROM follow WHERE LoggedID=?", $LoggedID);
+    foreach($follows as $follow)
+    {
+        $IsNotified = $follow["IsChecked"];
+    }
+
+    $NewNotifications = $Db::queryAll("SELECT * FROM follow WHERE LoggedID=? AND IsChecked=0", $LoggedID);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +25,7 @@
     <link rel="stylesheet" href="Notifications.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../UniversalCSS/UniversalStyles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../Nav/Nav.php"> 
+    <link rel="stylesheet" href="../Nav/Nav.php?v=<?php echo time(); ?>"> 
 </head>
 <body>
     <div class="whereamI">
@@ -21,5 +34,8 @@
     <?php
         include("../Nav/Nav.php");
     ?>
+    <div class="Notifications">
+
+    </div>
 </body>
 </html>
